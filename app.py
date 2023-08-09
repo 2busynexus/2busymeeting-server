ee092e0d7980494ddd029aac363cfbe32800c330
2busynexus <112109292+2busynexus@users.noreply.github.com>
2023-08-09 12:21:14 +0000

    made routes for join and create buttons and providing the return to front-end

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask, jsonify
+import uuid
 
 app = Flask(__name__)
+
+rooms = []
 
 @app.route('/ping')
 def flask_status():
 
     return jsonify('Flask is up')
+
+@app.route('/join')
+def join():
+
+  return jsonify('Joining a room')
+
+@app.route('/create')
+def create():
+  room_id = str(uuid.uuid4())
+  #print(room_id)
+  return jsonify(f'You created room: {room_id}')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
